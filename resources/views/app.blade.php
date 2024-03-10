904dbb127f6c011cc3ec1d653db289f6ddf62a96
Rodrigo Weiss <rwrodrigoa@gmail.com>
2024-03-10 19:16:37 -0300

    Add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,10 @@
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+    <link rel="icon" type="image/png" href="/favicon.png">
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
